1e99c7683a006447823e64402cac10e7c07236fc
jalal-26 <jalalalshofi@gmail.com>
2025-05-22 00:30:09 -0400

    Task-4.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import checker from 'vite-plugin-checker'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    checker({typescript:true}),
+    checker({ typescript: true }),
   ],
+  base: "/Task-4/",
 })
